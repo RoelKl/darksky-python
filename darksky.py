1cd9ce0a7648d5d97c5160ed06ccf65841209de0
Roel <46358015+RoelKl@users.noreply.github.com>
2019-08-20 22:52:59 +0200

    Updates in output

--- darksky.py
+++ darksky.py
@@ -1,8 +1,10 @@
 import requests
-import pandas as pd
+import json
+import datetime
 
 request_url = 'https://api.darksky.net/forecast/a4848c6efc250bc969f3cd235a53e9f3/52.449263,5.834519?units=si'
 result = requests.get(request_url).json()
-print(result)
-df = pd.DataFrame.from_dict(json_normalize(result), orient='columns')
-print(df.head())
+print(datetime.datetime.fromtimestamp(result['currently']['time']))
+for r in result['hourly']['data']:
+    print(datetime.datetime.fromtimestamp(r['time']), r['temperature'])
+
