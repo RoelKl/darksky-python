10e053f161f9edc0f59e0c1bfaf22df0deb7f217
Roel <46358015+RoelKl@users.noreply.github.com>
2019-08-20 20:34:15 +0200

    Update darksky.py

--- darksky.py
+++ darksky.py
@@ -1,5 +1,8 @@
 import requests
+import pandas as pd
 
 request_url = 'https://api.darksky.net/forecast/a4848c6efc250bc969f3cd235a53e9f3/52.449263,5.834519?units=si'
 result = requests.get(request_url).json()
 print(result)
+df = pd.DataFrame.from_dict(json_normalize(result), orient='columns')
+print(df.head())
