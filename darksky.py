48fc046f15323d0677af5f13c1f88288c81b03e2
Roel <46358015+RoelKl@users.noreply.github.com>
2019-08-21 08:54:05 +0200

    Extend the data pulled out of darksky

--- darksky.py
+++ darksky.py
@@ -4,7 +4,7 @@ import datetime
 
 request_url = 'https://api.darksky.net/forecast/a4848c6efc250bc969f3cd235a53e9f3/52.449263,5.834519?units=si'
 result = requests.get(request_url).json()
-print(datetime.datetime.fromtimestamp(result['currently']['time']))
+print(datetime.datetime.fromtimestamp(result['currently']['time']), result['currently']['temperature'],result['currently']['pressure'])
 for r in result['hourly']['data']:
-    print(datetime.datetime.fromtimestamp(r['time']), r['temperature'])
+    print(datetime.datetime.fromtimestamp(r['time']), r['temperature'],r['pressure'])
 
